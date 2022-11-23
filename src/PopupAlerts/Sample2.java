107eb99f4b734fa7071d53ad06d4cb51ae667c80
Shalu <Shalu@LAPTOP-DV34BMM4>
2022-11-23 12:44:41 +0530

    hi factorial

---
+++ src/PopupAlerts/Sample2.java
@@ -0,0 +1,19 @@
+package PopupAlerts;
+
+public class Sample2 {
+
+	public static void main(String[] args)
+	{
+		int num=5;
+		int fact =1;
+		for(int i= num;i>=1;i--)
+		{
+			fact=fact*i;
+			
+		}
+		System.out.println(fact);
+		// TODO Auto-generated method stub
+
+	}
+
+}
